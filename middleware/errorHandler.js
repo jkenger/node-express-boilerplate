75ea81b3bebe4f699e35621e31d1007ef0013798
Ken Gervacio <johnkgervacio@gmail.com>
2023-09-23 12:28:30 +0800

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,18 +1,11 @@
-const { logEvents } = require("./logger");
+// Type: Module
+import { StatusCodes } from "http-status-codes";
 
 const errorHandler = (err, req, res, next) => {
-  logEvents(
-    `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
-    "error.log"
-  );
-  const status = res.statusCode ? res.statusCode : 500;
-
-  res.status(status);
-
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
+  console.log(err);
+  const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  const msg = err.message || "Something went wrong, please try again later";
+  res.status(statusCode).json({ msg });
 };
 
-module.exports = { errorHandler };
+export default errorHandler;
